be0b8ad94e335a83b571142064930b4c3f8ac45b
Steve Rector <steverector27@gmail.com>
2022-11-28 15:07:47 -0800

    Adds working sphere movement based on force

--- Assets/Scripts/SphereSpawner.cs
+++ Assets/Scripts/SphereSpawner.cs
@@ -1,40 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class SphereSpawner : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+public class SphereSpawner : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private GameObject _spherePrefab;
     [SerializeField] private Transform _mouseTarget;
-    [SerializeField] private float _maxVelocity;
+    [SerializeField] private float _speed;
 
-    private Vector3 lastDragPosition;
     private GameObject _spawnedSphere;
+    private Vector3 _formattedMousePos;
+    private Vector3 _startPosition;
 
     // Update is called once per frame
     void Update()
     {
-        // sets up the mouseTarget to follow the cursor since the interfaces need an object to collect eventData
+        // sets up the mouseTarget to follow the cursor 
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 10;
 
-        Vector3 formattedMousePos = Camera.main.ScreenToWorldPoint(mousePos);
-        _mouseTarget.position = formattedMousePos;
+        _formattedMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        _mouseTarget.position = _formattedMousePos;
     }
 
-    public void OnDrag(PointerEventData eventData)
+    /// <summary>
+    /// Stores the starting position of the mouse when the mousebutton is first held down
+    /// </summary>
+    public void OnPointerDown(PointerEventData eventData)
     {
-        // keeps track of the distance between where the mouse was last frame vs this frame, updates as long as object is still being dragged
-        lastDragPosition = _mouseTarget.position;
+        _startPosition = _formattedMousePos;
     }
 
+    /// <summary>
+    /// When the mouse button is released, instantiates a sphere at that position and adds an impulse force on the sphere based on the direction of the mouse movement
+    /// </summary>
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("Drag ended");
         _spawnedSphere = Instantiate(_spherePrefab, _mouseTarget.position, Quaternion.identity);
-        
+        Rigidbody sphereRb = _spawnedSphere.GetComponent <Rigidbody>();
+
+        Vector3 force = transform.position - _startPosition;
+        force.Normalize();
+
+        sphereRb.AddForce(force * _speed, ForceMode.Impulse);
     }
 
-    public void OnPointerDown(PointerEventData eventData) { return; }
 }
